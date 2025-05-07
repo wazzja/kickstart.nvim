148123470074cf1fd70bfb2214882877a717043c
wazzja <wasja@velour.de>
2025-05-07 10:25:00 +0200

    trying to add new plugin

--- init.lua
+++ init.lua
@@ -268,6 +268,42 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'MoulatiMehdi/42norm.nvim',
+    config = function()
+      local norm = require '42norm'
+
+      norm.setup {
+        header_on_save = true,
+        format_on_save = true,
+        liner_on_change = true,
+      }
+
+      -- Press "F5" key to run the norminettea
+      vim.keymap.set('n', '<F5>', function()
+        norm.check_norms()
+      end, { desc = 'Update 42norms diagnostics', noremap = true, silent = true })
+
+      vim.keymap.set('n', '<C-f>', function()
+        norm.format()
+      end, { desc = 'Format buffer on 42norms', noremap = true, silent = true })
+
+      vim.keymap.set('n', '<F1>', function()
+        norm.stdheader()
+      end, { desc = 'Insert 42header', noremap = true, silent = true })
+
+      -- create your commands
+      vim.api.nvim_create_user_command('Norminette', function()
+        norm.check_norms()
+      end, {})
+      vim.api.nvim_create_user_command('Format', function()
+        norm.format()
+      end, {})
+      vim.api.nvim_create_user_command('Stdheader', function()
+        norm.stdheader()
+      end, {})
+    end,
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
